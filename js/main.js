cfb9b9c5de0b9063c039a56bcec781e3b61eb4cf
diamant1991 <diamant1988@mail.ru>
2015-01-24 17:27:21 +0200

    add other files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-$( document ).ready(function() { 	$('#subscription_email').focus(function(){
+$( document ).ready(function() {
+ 	$('#subscription_email').focus(function(){
  		if($(this).val()=='Ваш Email')
  	       $(this).val('')
  	})
@@ -17,4 +18,20 @@ $( document ).ready(function() { 	$('#subscription_email').focus(function(){
  		if($(this).val()=='')
  	       $(this).val('Найти в каталоге')
  	})
+
+ 	$('.indication').click(function(){ // при клике по заголовку спойлера
+				var show = $(this).attr('show'); // проверяем атрибут, в котором записано - отображен спойлер или скрыт
+				if(show == 1){ // если отображен, то прячем
+					$(this).attr('show', 0);
+					$('.spoiler-body').slideUp(100);
+					$(this).css("background","url(images/arrow-bottom.png)")
+					
+				}else{ // если спрятан, то показываем
+					$(this).attr('show', 1);
+					$('.spoiler-body').slideDown(300);
+					$(this).css("background","url(images/arrow-up.png)")
+				}
+			});
+
+
 });
