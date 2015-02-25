259d6a824e772fe834ff6d258993b8e0c22915fe
diamant1991 <diamant1988@mail.ru>
2015-02-25 15:27:36 +0200

    carousel

---
+++ js/carousel.js
@@ -0,0 +1,30 @@
+//Обработка клика на стрелку вправо
+$(document).on('click', "#goods_arrow",function(){ 
+    var carusel = $(this).parents('#goods_info_images');
+    right_carusel(carusel);
+    return false;
+});
+//Обработка клика на стрелку влево
+$(document).on('click',".carousel-button-left",function(){ 
+    var carusel = $(this).parents('.carousel');
+    left_carusel(carusel);
+    return false;
+});
+function left_carusel(carusel){
+   var block_width = $(carusel).find('.carousel-block').outerWidth();
+   $(carusel).find(".carousel-items .carousel-block").eq(-1).clone().prependTo($(carusel).find(".carousel-items")); 
+   $(carusel).find(".carousel-items").css({"left":"-"+block_width+"px"});
+   $(carusel).find(".carousel-items .carousel-block").eq(-1).remove();    
+   $(carusel).find(".carousel-items").animate({left: "0px"}, 200); 
+    
+}
+function right_carusel(carusel){
+   var block_width = $(carusel).find('.carousel-block').outerWidth();
+   $(carusel).find(".carousel-items").animate({left: "-"+ block_width +"px"}, 200, function(){
+      $(carusel).find(".carousel-items .carousel-block").eq(0).clone().appendTo($(carusel).find(".carousel-items")); 
+      $(carusel).find(".carousel-items .carousel-block").eq(0).remove(); 
+      $(carusel).find(".carousel-items").css({"left":"0px"}); 
+   }); 
+}
+ 
+
